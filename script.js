c409427a07e26c6f017e47d29360f1572e4cd662
Khalid Hossain <67034266+thespeck@users.noreply.github.com>
2024-03-13 17:55:53 +0600

    Added files

---
+++ script.js
@@ -0,0 +1,74 @@
+/**
+ * @type {HTMLInputElement}
+*/
+let input = document.getElementById('url-input');
+/**
+ * @type {NodeListOf<HTMLImageElement>} Nodelist of images
+ */
+let images = document.querySelectorAll('.thumbnail');
+/**
+ * @type {HTMLButtonElement} button
+*/
+let button = document.getElementById('input-btn');
+/**
+ * @type {HTMLDivElement} div
+ */
+let display = document.getElementById('display');
+/**
+ * @type {HTMLLinkElement} favicon
+ */
+let link = document.querySelector("link[rel~='icon']");
+
+
+input.addEventListener('focus', () => {input.select()});
+input.addEventListener('change', processThumbnail);
+input.addEventListener('input', processThumbnail);
+button.addEventListener('click', processThumbnail);
+
+processThumbnail();
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+
+if (matchMedia && matchMedia('(prefers-color-scheme: dark)').matches) {
+    link.href = "./favicon-dark.png";
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if (e.matches) {
+        link.href = "./favicon-dark.png";
+    } else {
+        link.href = "./favicon-light.png";
+    }
+});
+
+function processThumbnail() {
+    let url = input.value.trim();
+    let id = parseID(url);
+    if (id === undefined) return;
+    let thumbURLS = [
+        `https://i.ytimg.com/vi_webp/${id}/maxresdefault.webp`,
+        `https://i.ytimg.com/vi_webp/${id}/sddefault.webp`,
+        `https://i.ytimg.com/vi_webp/${id}/hqdefault.webp`,
+        `https://i.ytimg.com/vi_webp/${id}/mqdefault.webp`,
+    ];
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = thumbURLS[i];
+    }
+    display.classList.remove("hidden");
+}
+
+/**
+ * Parses video ID from YouTube URL
+ * @param {string} url YouTube video URL
+ * @return {string | undefined} YouTube video ID
+*/
+function parseID(url) {
+    let matches = url.match(/[/=]([0-9a-zA-Z-_]{11})[?&]?/);
+    if (matches && matches.length) {
+        return matches[1];
+    }
+    return;
+}
